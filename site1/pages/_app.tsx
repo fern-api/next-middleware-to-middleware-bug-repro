a12d3c1c220029ece4f47fb863fd799b3f9cc056
Andrew Jiang <andrew@buildwithfern.com>
2024-12-18 08:53:42 -0500

    move hooks

--- site1/pages/_app.tsx
+++ site1/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useInterceptNextDataHref } from "./hooks/use-intercept-next-page-href";
+import { useInterceptNextDataHref } from "../hooks/use-intercept-next-page-href";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   useInterceptNextDataHref({ router: router, basePath: "/testing" });
